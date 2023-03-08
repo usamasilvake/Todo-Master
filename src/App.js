83d1ca5e944c9be9814737008b98b888c3059b74
usama <usamasilvake@gmail.com>
2023-03-07 21:33:33 -0800

    added css

--- src/App.js
+++ src/App.js
@@ -5,10 +5,35 @@ import TodoResult from './compoents/TodoResult';
 import { collection, query, onSnapshot, Timestamp, addDoc, deleteDoc, doc, updateDoc } from 'firebase/firestore';
 import { db } from './firebase-cong';
 import { Button, TextField } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+  inputtext: {
+    width: '28rem',
+    '& .MuiInputBase-input': {
+      fontSize: '1.2rem',
+      color: '#333',
+    },
+    '& .MuiInputLabel-root': {
+      fontSize: '1.3rem', fontWeight: 'bold',
+      color: 'white',
+    },
+    '& .MuiInputBase-root': {
+      borderBottom: '2px solid yellow',
+    }
+
+  },
+  inputbutton:{
+    margin: '10px 0 0px 30px',
+  }
+});
+
+
 
 function App() {
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState('');
+  const formstyle = useStyles();
 
   // create todo from firebase
   const createTodo = async (e) => {
@@ -25,7 +50,7 @@ function App() {
     }
   }
 
- // Read data from firebase
+  // Read data from firebase
   useEffect(() => {
     const q = query(collection(db, 'todotable'));
     const unsub = onSnapshot(q, (querySnapshot) => {
@@ -38,7 +63,7 @@ function App() {
     return () => unsub();
   }, []);
 
-       // update todo from firebase
+  // update todo from firebase
   const toggleComplete = async (todo) => {
     await updateDoc(doc(db, 'todotable', todo.id), {
       completed: !todo.completed,
@@ -51,17 +76,14 @@ function App() {
 
   return (
     <div>
-      <form className='todo_input_form' style={{ textAlign: 'center', padding: '2rem' }} onSubmit={createTodo}>
-        <TextField color="warning" sx={{ width: '28rem', '& .MuiInputBase-input': { fontSize: '1.2rem', color: '#333', },
-         '& .MuiInputLabel-root': {fontSize: '1.3rem', fontWeight: 'bold', color: 'white',}, '& .MuiInputBase-root': {
-          borderBottom: '2px solid yellow',},}}
-          id="standard-search" label="Search Todo" type="search" variant="standard" placeholder="Enter message"
-          value={input} onChange={(e) => setInput(e.target.value)} />
+      <form className='todo_form' onSubmit={createTodo}>
+        <TextField className={formstyle.inputtext} color="warning" id="standard-search" label="Search Todo"
+          type="search" variant="standard" placeholder="Enter message" value={input}
+          onChange={(e) => setInput(e.target.value)} />
 
-        <Button sx={{margin:'10px 0 0px 30px'}} type="submit" variant="contained" size="large">add</Button>
+        <Button className={formstyle.inputbutton} type="submit" variant="contained" size="large">add</Button>
       </form>
       {todos.map((todo, index) => (
-
         <TodoResult todo={todo} key={index} toggleComplete={toggleComplete} deleteTodo={deleteTodo} />
       ))}
     </div>
